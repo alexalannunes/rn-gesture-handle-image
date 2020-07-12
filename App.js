5902553421204700913e91c0a13481695daf7a0a
Alex Alan Nunes <alexalannunes@gmail.com>
2020-07-12 18:49:38 -0300

    fix blank lines

--- App.js
+++ App.js
@@ -1,45 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-
 import React from "react";
-
 import { StyleSheet, Image, View, Animated, Dimensions } from "react-native";
-
 import { PanGestureHandler } from "react-native-gesture-handler";
 
 const { width } = Dimensions.get("window");
-
 const url = "https://humanrights.uconn.edu/wp-content/uploads/sites/2330/2018/01/Akaslan-2018-Vertical-.jpg";
-
 const SIZE = 50;
-
 const HALF_SIZE = SIZE / 2;
-
 const MIN = 0;
-
 const PROGRESS = width - 20;
-
 const MAX = PROGRESS - SIZE;
-
 const CENTER = PROGRESS / 2 - HALF_SIZE;
 
 export default function App() {
   let tx = new Animated.Value(CENTER);
-
   let offset = CENTER;
-
   let _onPanGestureEvent = ({ nativeEvent: { x, y, translationX, translationY, state } }) => {
     // console.log({ x, y, translationX, translationY, state });
-
     tx.setValue(translationX);
   };
 
   let stateChange = ({ nativeEvent }) => {
     const { translationX } = nativeEvent;
-
     offset += translationX;
-
     tx.setOffset(offset);
-
     // console.log(nativeEvent);
   };
 
@@ -54,28 +38,20 @@ export default function App() {
             {
               rotateY: tx.interpolate({
                 inputRange: [MIN, CENTER, MAX],
-
                 outputRange: ["-30deg", "0deg", "30deg"],
-
                 extrapolate: "clamp",
               }),
             },
-
             {
               scale: tx.interpolate({
                 inputRange: [MIN, CENTER, MAX],
-
                 outputRange: [1.2, 1, 1.2],
-
                 extrapolate: "clamp",
               }),
             },
           ],
-
           backgroundColor: "red",
-
           width: width,
-
           height: width + width * 0.5,
         }}
       ></Animated.Image>
@@ -88,20 +64,15 @@ export default function App() {
                 {
                   translateX: tx.interpolate({
                     inputRange: [MIN, CENTER, MAX],
-
                     outputRange: [MIN, CENTER, MAX],
-
                     extrapolate: "clamp",
                   }),
                 },
               ],
 
               width: SIZE,
-
               height: SIZE,
-
               backgroundColor: "#333",
-
               borderRadius: SIZE,
             }}
           ></Animated.View>
@@ -114,9 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
     backgroundColor: "#fff",
-
     justifyContent: "center",
   },
 });
